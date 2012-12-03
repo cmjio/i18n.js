f6e8d852182960178a64654002ed1cf2a883175f
Chris Johnson <chrisjohnson@rehabstudio.com>
2012-12-03 16:00:52 +0000

    initial commit with base plugin

---
+++ jquery.i18n.js
@@ -0,0 +1,43 @@
+(function($) {
+// What does the pluginName plugin do?
+$.fn.i18n = function(options) {
+
+  if (!this.length) { return this; }
+
+  var opts = $.extend(true, {}, $.fn.i18n.defaults, options);
+
+  var valueForKey = function(key, data) {
+    var keys, value, _i, _len;
+    keys = key.split(/\./);
+    value = data;
+    for (_i = 0, _len = keys.length; _i < _len; _i++) {
+      key = keys[_i];
+      value = value != null ? value[key] : null;
+    }
+    return value;
+  };
+
+  this.each(function() {
+    var $this = $(this);
+    var language = opts.forceLanguage || window.navigator.userLanguage || window.navigator.language;
+	var fileName = opts.path +'/'+ opts.namespace +'_'+ language +'.json';
+
+	$.getJSON(fileName, function(data) {
+		var key = $this.data('translate');
+		value = valueForKey(key, data);
+		$this.html(value);
+	});
+    
+  });
+
+  return this;
+};
+
+// default options
+$.fn.i18n.defaults = {
+  namespace: 'i18n',
+  path: 'js/lang',
+  forceLanguage: null
+};
+
+})(jQuery);
